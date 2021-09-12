81910cc4a64ca1137edcf8889a6cddedd1e51df7
sursev07 <simangunsong.seven@gmail.com>
2021-09-12 22:55:17 +0700

    add eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
       commonjs: true,
       es2021: true,
     },
-    extends: [
-      'airbnb-base',
-    ],
     parserOptions: {
       ecmaVersion: 12,
     },
     rules: {
-    },
+      semi: ["error", "always"],
+      quotes: ["error", "double"]
+    }
   };
