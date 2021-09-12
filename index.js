3ce4702e376e91f325228f9f7e1981f51aac4b33
sursev07 <simangunsong.seven@gmail.com>
2021-09-12 22:12:07 +0700

    finishing task

--- index.js
+++ index.js
@@ -1,21 +1,102 @@
 function splitString(str) {
   // Your code here
+  let output = []
+  let number = ''
+
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] === ' ' || str[i] === undefined) {
+      output.push(number)
+      number = ''
+    } else {
+      number += str[i]
+    }
+  }
+
+  return output
 }
 
 function reverseOddIndex(arr) {
   // Your code here
+  let output = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 1) {
+      let reverse = ''
+  
+      for (let j = arr[i].length - 1; j >= 0; j--) {
+        reverse += arr[i][j]
+      }
+  
+      output.push(reverse)
+    } else {
+      output.push(arr[i])
+    }
+  }
+
+  return output
 }
 
 function decryptElement(arr) {
   // Your code here
+  let output = []
+  let symbol = '!@#$%^&*()'
+
+  for (let i = 0; i < arr.length; i++) {
+    let decrypt = ''
+    
+    for (let j = 0; j < arr[i].length; j++) {
+      let index = Number(arr[i][j])
+
+      if (i % 2 === 1) {
+        if (index === 0) {
+          index = 9
+        } else {
+          index -= 1
+        }
+      }
+
+      decrypt += symbol[index]
+    }
+
+    output.push(decrypt)
+  }
+
+  return output
 }
 
 function groupingSymbol(arr) {
   // Your code here
+  let output = []
+
+  for (let i = 0; i < arr.length; i += 2) {
+    let temp = []
+    for (let j = 0; j < 2; j++) {
+      temp.push(arr[i + j])
+    }
+    output.push(temp)
+  }
+
+  return output
 }
 
 function serialValidator(str) {
   // Your code here
+  if (String(str).length < 9) {
+    return 'Harus lebih dari 9 karakter'
+  }
+
+  let splitted = splitString(str)
+  let reversed = reverseOddIndex(splitted)
+  let decrypted = decryptElement(reversed)
+  let grouped = groupingSymbol(decrypted)
+
+  for (let i = 0; i < grouped.length; i++) {
+    if (grouped[i][0] !== grouped[i][1]) {
+      return 'Invalid serial number'
+    }
+  }
+
+  return 'Serial number is valid'
 }
 
 console.log(serialValidator("1234"));
